338d7ac706245f12350b2300e2dbee5bc7508aca
GitHub Action <action@github.com>
2021-03-15 18:53:01 +0000

    Updating generated outputs

---
+++ python/mappings.py
@@ -0,0 +1,57 @@
+# Auto generated from mappings.yaml by pythongen.py version: 0.9.0
+# Generation date: 2021-03-15 18:52
+# Schema: mappings
+#
+# id: https://w3id.org/linkml/mappings
+# description: LinkML model for mappings
+# license: https://creativecommons.org/publicdomain/zero/1.0/
+
+import dataclasses
+import sys
+import re
+from typing import Optional, List, Union, Dict, ClassVar, Any
+from dataclasses import dataclass
+from biolinkml.meta import EnumDefinition, PermissibleValue, PvFormulaOptions
+
+from biolinkml.utils.slot import Slot
+from biolinkml.utils.metamodelcore import empty_list, empty_dict, bnode
+from biolinkml.utils.yamlutils import YAMLRoot, extended_str, extended_float, extended_int
+if sys.version_info < (3, 7, 6):
+    from biolinkml.utils.dataclass_extensions_375 import dataclasses_init_fn_with_kwargs
+else:
+    from biolinkml.utils.dataclass_extensions_376 import dataclasses_init_fn_with_kwargs
+from biolinkml.utils.formatutils import camelcase, underscore, sfx
+from biolinkml.utils.enumerations import EnumDefinitionImpl
+from rdflib import Namespace, URIRef
+from biolinkml.utils.curienamespace import CurieNamespace
+from . types import Uriorcurie
+from biolinkml.utils.metamodelcore import URIorCURIE
+
+metamodel_version = "1.7.0"
+
+# Overwrite dataclasses _init_fn to add **kwargs in __init__
+dataclasses._init_fn = dataclasses_init_fn_with_kwargs
+
+# Namespaces
+IAO = CurieNamespace('IAO', 'http://purl.obolibrary.org/obo/IAO_')
+OIO = CurieNamespace('OIO', 'http://www.geneontology.org/formats/oboInOwl#')
+LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
+RDF = CurieNamespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+RDFS = CurieNamespace('rdfs', 'http://www.w3.org/2000/01/rdf-schema#')
+SKOS = CurieNamespace('skos', 'http://www.w3.org/2004/02/skos/core#')
+XSD = CurieNamespace('xsd', 'http://www.w3.org/2001/XMLSchema#')
+DEFAULT_ = LINKML
+
+
+# Types
+
+# Class references
+
+
+
+
+# Enumerations
+
+
+# Slots
+
