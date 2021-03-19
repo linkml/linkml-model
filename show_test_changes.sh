0b828c65c75d808395a7e920faa08528b894e914
hsolbrig <solbrig@jhu.edu>
2021-03-19 16:38:19 -0500

    Add slashes at the end of directories
    See: https://stackoverflow.com/questions/12920652/git-update-index-assume-unchanged-returns-fatal-unable-to-mark-file/39434567#39434567

--- show_test_changes.sh
+++ show_test_changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make all of the test output changes visible to git
 . ./targets.sh
-git update-index --no-assume-unchanged $TGTS
+git update-index --no-assume-unchanged `echo $TGTS | sed 's/ /\/ /g'`/
