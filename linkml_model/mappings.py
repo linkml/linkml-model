d0d3c4c08bb6992888efbf60ddcdfd3baa383307
GitHub Action <action@github.com>
2021-03-22 18:59:36 +0000

    Updating generated outputs

--- linkml_model/mappings.py
+++ linkml_model/mappings.py
@@ -1,5 +1,5 @@
 # Auto generated from mappings.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-03-22 18:03
+# Generation date: 2021-03-22 18:59
 # Schema: mappings
 #
 # id: https://w3id.org/linkml/mappings
