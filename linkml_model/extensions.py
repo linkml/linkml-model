5a72e5af4373af87f263670f450b1d306aac565e
GitHub Action <action@github.com>
2021-03-24 14:18:21 +0000

    Updating generated outputs

--- linkml_model/extensions.py
+++ linkml_model/extensions.py
@@ -1,5 +1,5 @@
 # Auto generated from extensions.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-03-23 22:31
+# Generation date: 2021-03-24 14:16
 # Schema: extensions
 #
 # id: https://w3id.org/linkml/extensions
--- linkml_model/mappings.py
+++ linkml_model/mappings.py
@@ -1,5 +1,5 @@
 # Auto generated from mappings.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-03-23 22:31
+# Generation date: 2021-03-24 14:17
 # Schema: mappings
 #
 # id: https://w3id.org/linkml/mappings
