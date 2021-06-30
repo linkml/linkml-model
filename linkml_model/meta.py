493170cc78a2beb3a0e1b80f3a2193fd6155ef30
GitHub Action <action@github.com>
2021-06-30 16:54:41 +0000

    Updating generated outputs

--- linkml_model/meta.py
+++ linkml_model/meta.py
@@ -1,5 +1,5 @@
 # Auto generated from meta.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-06-30 16:51
+# Generation date: 2021-06-30 16:54
 # Schema: meta
 #
 # id: https://w3id.org/linkml/meta
