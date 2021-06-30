e36c05fa8e6d49d638f3aa066f3881f7e584f150
GitHub Action <action@github.com>
2021-06-30 17:04:58 +0000

    Updating generated outputs

--- linkml_model/extensions.py
+++ linkml_model/extensions.py
@@ -1,5 +1,5 @@
 # Auto generated from extensions.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-06-30 16:54
+# Generation date: 2021-06-30 17:04
 # Schema: extensions
 #
 # id: https://w3id.org/linkml/extensions
