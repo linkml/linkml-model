813d73abaf1271a6fddf22556270549bd33ac408
hsolbrig <solbrig@jhu.edu>
2021-08-18 11:41:46 -0500

    Update make and build files

--- hide_test_changes.sh
+++ hide_test_changes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Make all of the test output files invisible to git
-echo git update-index --assume-unchanged `git status -s docs | sed 's/^[[:space:]]*[^[:space:]]*[[:space:]]*//'`
-echo git update-index --assume-unchanged `git status -s linkml_model | sed 's/^[[:space:]]*[^[:space:]]*[[:space:]]*//' | grep -v \/model\/`
+git update-index --assume-unchanged `git status -s docs | sed 's/^[[:space:]]*[^[:space:]]*[[:space:]]*//'`
+git update-index --assume-unchanged `git status -s linkml_model | sed 's/^[[:space:]]*[^[:space:]]*[[:space:]]*//' | grep -v \/model\/`
