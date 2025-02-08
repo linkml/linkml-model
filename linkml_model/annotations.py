c206ba7ad3b035b21f45d008132ffab89e2e3438
Chris Mungall <cjm@berkeleybop.org>
2025-02-07 16:29:37 -0800

    Generated downstream artefacts

--- linkml_model/annotations.py
+++ linkml_model/annotations.py
@@ -1,5 +1,5 @@
 # Auto generated from annotations.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-02-07T17:29:22
+# Generation date: 2025-02-07T16:22:05
 # Schema: annotations
 #
 # id: https://w3id.org/linkml/annotations
